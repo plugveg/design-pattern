bcab99362c716bb374b7cc68641a6e5cb716172a
Setsuma <71630536+plugveg@users.noreply.github.com>
2023-05-29 20:51:08 +0200

    feat(): creation de la enum ressources

---
+++ src/EnumRessources.java
@@ -0,0 +1,22 @@
+public enum EnumRessources {
+    BOIS(10, "leBois"),
+    PIERRE(10, "laPierre"),
+    OR(10, "L'Or"),
+    MIAM(10, "La graille");
+
+    private final int cout;
+    private final String signification;
+
+    EnumRessources(int monCout, String maSignification ){
+        this.cout = monCout;
+        this.signification = maSignification;
+    }
+
+    public int getCout() {
+        return cout;
+    }
+
+    public String getSignification() {
+        return signification;
+    }
+}
